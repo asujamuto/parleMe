3c4c621a09e56cfddfb09414af4004fa8727fa79
asujamuto <sokolowskipiotr297@protonmail.com>
2025-03-11 19:30:34 +0100

    amplify added

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "parleme": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
